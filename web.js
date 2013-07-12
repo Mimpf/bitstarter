1c5797d75cf2889118d8f7d5a7dfaa9f38419da1
Peter Weigt <peter.weigt@gmail.com>
2013-07-12 20:15:18 +0000

    Read from index.html file

--- web.js
+++ web.js
@@ -1,9 +1,21 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var filename = "index.html";
+
+function readFS(filename){
+	var file = fs.readFile(filename, 'utf-8',function (err, data) {
+  				if (err) throw err;
+  					console.log(data);
+				});
+	return file;
+	
+	}
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  response.send(readFS());
 });
 
 var port = process.env.PORT || 5000;
